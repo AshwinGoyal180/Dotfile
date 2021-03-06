33c6b0700f6102a5e072d580a64ece90aa6ed3d2
Ashwin <ashwingoyal180@gmial.com>
2021-03-07 00:24:48 +0530

    tri

--- .config/conky/ashwin/zpoolstatus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-zpool status > ~/.config/conky/ashwin/zpool_status 
-if [ ! -z `cat ~/.config/conky/ashwin/zpool_status | grep "DEGRADED"` ]; then 
-	cat ~/.config/conky/ashwin/zpool_status
-fi
